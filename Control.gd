8eb73000fef1b66d11b82629c92de1ae30b9f0ab
Lisandro Lorea <lisandrolorea@gmail.com>
2023-02-21 04:41:39 -0300

    Automatically create UI based on param list

--- Control.gd
+++ Control.gd
@@ -11,14 +11,28 @@ const Responses = NetAPI.Responses
 const COMMAND_INFO = NetAPI.COMMAND_INFO
 # /IMPORT NetAPI
 
+var param_update_handlers = {}
+
+onready var param_list = $"%ParamList"
+onready var message_log = $"%MessageLog"
+
 func _ready():
-	print("hola")
-	peer.connect_to_host("127.0.0.1", port)
-	while (peer.get_status() != peer.STATUS_CONNECTED):
-		yield(get_tree(), "idle_frame")
-	
-	peer.put_var([Commands.GLOBAL_PARAM_GET, "size"])
-	peer.put_var([Commands.GLOBAL_PARAM_GET, "speed"])
+	while not peer.is_connected_to_host():
+		peer.connect_to_host("127.0.0.1", port)
+		while (peer.get_status() == peer.STATUS_CONNECTING):
+			message_log.text = "Connecting " + [".", "..", "..."][randi()%3] 
+			yield(get_tree(), "idle_frame")
+		if peer.get_status() == peer.STATUS_ERROR:
+			message_log.text = "Error connecting. Retrying in 5s"
+			yield(get_tree().create_timer(5), "timeout")
+	message_log.text = "Connected. Getting param list.."
+	peer.put_var([Commands.GLOBAL_PARAM_LIST])
+	# Some tests. Should fail
+	peer.put_var([10, "comova", "todo", "amigo"])
+	peer.put_var([Commands.GLOBAL_PARAM_GET, "bad", 10, "params"])
+	peer.put_var([Commands.GLOBAL_PARAM_GET, 0])
+
+
 
 func _process(delta):
 	if not peer.is_connected_to_host():
@@ -30,31 +44,70 @@ func _process(delta):
 		var response : NetAPI.Response = NetAPI.Response.from_array(packet)
 		match response.type:
 			Responses.ERROR:
-				prints("Error", response.data)
+				prints("Error", response.data.message)
+				message_log.text = "Error: %s" % response.data.message
 			Responses.BUG:
 				prints("Unknown Error", response.data)
+				message_log.text = "Unknown Error. Please Report. %s" % response.data.message
+			Responses.INCORRECT_PARAM_COUNT:
+				prints("Incorrect param count", response.data.message)
+				message_log.text = response.data.message
+			Responses.INCORRECT_PARAM_TYPE:
+				prints("Ivalid param type", response.data.message)
+				message_log.text = response.data.message
+			Responses.INVALID_COMMAND:
+				prints("Invalid command", response.data.message)
+				message_log.text = response.data.message
 			Responses.OK:
 				var event = response.data.event
 				match event:
 					"global_param_changed":
 						print(response.data.param)
-						match response.data.param.keys()[0]:
-							"speed":
-								$VBoxContainer/HBoxContainer2/SpeedValue.text = str(response.data.param.values()[0])
-							"size":
-								$VBoxContainer/HBoxContainer/SizeValue.text = str(response.data.param.values()[0])
-
+						_on_param_get(response.data.param[0], response.data.param[1])
+					"global_param_list_ready":
+						_populate_params_ui(response.data.list)
 
 func _exit_tree():
 	peer.disconnect_from_host()
 
+func _populate_params_ui( property_list : Array ):
+	for prop in property_list:
+		var param_box = HBoxContainer.new()
+		
+		var name_label = Label.new()
+		name_label.name = "name"
+		name_label.text = (prop.name as String).trim_prefix("param_").capitalize()
+		print(name_label.text)
+		param_box.add_child(name_label)
+		
+		var value_spinbox = SpinBox.new()
+		value_spinbox.name = "value"
+		if prop.hint == PROPERTY_HINT_RANGE:
+			var values = (prop.hint_string as String).split(",")
+			value_spinbox.min_value = float(values[0])
+			value_spinbox.max_value = float(values[1])
+			value_spinbox.rounded = prop.type == TYPE_INT
+			value_spinbox.step = float(values[2]) if values.size() == 3 else 0
+		value_spinbox.connect("value_changed", self, "_on_param_set", [prop.name])
+		param_box.add_child(value_spinbox)
+		
+		param_list.add_child(param_box)
+		param_update_handlers[prop.name] = param_box
+		peer.put_var([Commands.GLOBAL_PARAM_GET, prop.name])
+	message_log.text = "Ready"
 
 
-func _on_SpeedButton_pressed():
-	peer.put_var([Commands.GLOBAL_PARAM_SET, "speed", float($VBoxContainer/HBoxContainer2/SpeedValue.text)])
 
 
 
-func _on_SizeButton_pressed():
-	peer.put_var([Commands.GLOBAL_PARAM_SET, "size", float($VBoxContainer/HBoxContainer/SizeValue.text)])
-
+func _on_param_set(value, key):
+	peer.put_var([Commands.GLOBAL_PARAM_SET, key, value])
+	
+func _on_param_get(key, value):
+	var handler : HBoxContainer = param_update_handlers.get(key)
+	if not  handler:
+		return
+	var value_node : Range = handler.get_node("value")
+	value_node.set_block_signals(true)
+	value_node.value = value
+	value_node.set_block_signals(false)
